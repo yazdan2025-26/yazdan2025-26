4a131392d4efa23e0f51ccc6ca238d0a773a581f
yazdan <yazdangardize@gmail.com>
2025-06-17 13:35:16 +0200

    new pc

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var random  = new Random();
+int randomnumber = random.Next(1,200);
+
+Console.Write("feef me nummer:");
+string? input = Console.ReadLine(); 
+
+Console.WriteLine(input);
+
+if (Number==randomnumber)
+{
+    Console.WriteLine("WIRITE OKE");
+}
+else
+{
+    Console.WriteLine(" write false");
+}
